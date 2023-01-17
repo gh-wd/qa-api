bc515a8c160227740464caa07390f4426e8f8be2
devrisby <118779687+devrisby@users.noreply.github.com>
2023-01-16 23:11:53 -0500

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('QA api')
+    .setDescription('Serves collections of question-answer resources')
+    .setVersion('1.0')
+    .addTag("qa's")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,12 +1,15 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 }
